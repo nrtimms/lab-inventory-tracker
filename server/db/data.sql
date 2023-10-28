4b0ca68f5affec3f0a501bafc3033d659c4653aa
nrtimms <nataliertimms18@gmail.com>
2023-10-28 12:06:45 -0700

    add authorization routes

--- server/db/data.sql
+++ server/db/data.sql
@@ -2,7 +2,7 @@ CREATE DATABASE lab_inventory_tracker;
 
 -- users table 
 CREATE TABLE users(
-  user_id serial primary key,
+  user_id SERIAL PRIMARY KEY,
   user_name VARCHAR(255) NOT NULL,
   user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL
@@ -10,14 +10,14 @@ CREATE TABLE users(
 
 -- chemicals table
 create table chemicals(
-  chem_id serial primary key,
-  user_id serial,
-  chem_name varchar(255),
-  molar_mass varchar(255),
-  amount varchar(255),
-  chem_loc varchar(255),
-  vendor_name varchar(255),
-  cat_num varchar(255),
-  cas_num varchar(255),
+  chem_id SERIAL PRIMARY KEY,
+  user_id SERIAL,
+  chem_name VARCHAR(255),
+  molar_mass VARCHAR(255),
+  current_amount VARCHAR(255),
+  chem_loc VARCHAR(255),
+  vendor_name VARCHAR(255),
+  cat_num VARCHAR(255),
+  cas_num VARCHAR(255),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 //routes
+app.use("/authentication", require("./routes/auth"));
+app.use("/chemicals", require("./routes/chem"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
